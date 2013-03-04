e786cc58dcca00c66ea825e50f8e4e193e606c14
daveputman77 <daveputman77@gmail.com>
2013-03-04 15:44:31 -0500

    first commit

---
+++ angryboss.rb
@@ -0,0 +1,7 @@
+puts "What do you want, employee filth?"
+want = gets.chomp
+puts "what do you mean you want "+ want +"? TOO BAD YOU\'RE FIRED!"
+
+if want == '[]'
+  puts "TOO BAD YOU ARE FIRED!"
+end
